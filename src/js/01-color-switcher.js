8ead7989341f084fe9fb6e50123e4b6fa1859975
eugene <j.golyanov9992@gmail.com>
2023-11-05 20:08:21 +0100

    a

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,17 @@
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyGetToWork = () => document.querySelector('body').style.backgroundColor = `${getRandomHexColor()}`
+let timeUpdateColor = null;
+startBtn.addEventListener('click', () => {
+    bodyGetToWork();
+    timeUpdateColor = setInterval(bodyGetToWork, 1000);
+    startBtn.style.pointerEvents = 'none';
+})
+stopBtn.addEventListener('click', () => {
+    clearInterval(timeUpdateColor);
+    startBtn.style.pointerEvents = 'auto';
+})
